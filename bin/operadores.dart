534fe809664e332d29615259d20f8f1ad506f84c
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2024-01-24 17:06:36 -0500

    Operadores aritmeticos relacionales logicos

---
+++ bin/operadores.dart
@@ -0,0 +1,31 @@
+import 'package:operadores/operadores.dart' as operadores;
+
+void main(List<String> arguments) {
+  //Operadores Aritmeticos - para realizar operaciones matematicas
+  print(10 + 20);
+  print(10 - 3);
+  print(2 / 1.5);
+  print(3 * 10);
+
+  //Operadores relacionales - para comprobar dos datos del mismo tipo, resultado valor booleano
+  print('Fabian' == 'fabian');
+  print('Fabian' != 'fabian');
+  print(10 > 2);
+  print(10 < 2);
+  print(5 >= 6);
+  print(5 <= 6);
+
+  //Operadores logicos -
+  print(10 < 20 && 'Fabian'.length == 6);
+  print(10 > 20 && 'Fabian'.length == 6);
+  final contains = 'Fabian'.contains('a');
+  print(contains);
+  print(!contains);
+
+  final String? email = 'test@gmail.com';
+  //final next = email.contains('admin') && email.endsWith('@gmail.com') || email.endsWith('@yahoo.com');
+
+  if (email != null && email.length > 0) {}
+
+  //Operadores de asignación
+}
